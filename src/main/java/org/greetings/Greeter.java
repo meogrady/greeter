8a6d774979d1f506f1598f0dfd7044b837a187fb
Mark O'Grady <meogady@yahoo.com>
2019-05-20 16:31:51 -0700

    clean up ignored files.

--- src/main/java/org/greetings/Greeter.java
+++ src/main/java/org/greetings/Greeter.java
@@ -1,8 +1,11 @@
 package org.greetings;
 
-
+/**
+ * This is a change to greeter
+ */
 public class Greeter {
     public static void main (String [] args) {
+
         System.out.println("Greetings!!!");
     }
 }
