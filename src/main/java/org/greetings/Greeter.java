bf794de5389d4734881753ddcb834a125b979f36
Mark O'Grady <meogady@yahoo.com>
2019-05-27 07:21:13 -0700

    Added code to prompt user for first name, and display greeting.

--- src/main/java/org/greetings/Greeter.java
+++ src/main/java/org/greetings/Greeter.java
@@ -1,11 +1,16 @@
 package org.greetings;
 
+import java.util.Scanner;
+
 /**
  * This is a change to greeter. Master repo
  */
 public class Greeter {
     public static void main (String [] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first name: ");
+        String firstName = scan.next();
 
-        System.out.println("Hi, Greetings!");
+        System.out.println("Hi, Greetings " +  firstName + "!");
     }
 }
